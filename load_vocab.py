0664e0704c010e9bfe2003572880cf91e0ef445d
chengniu <chengniu@gmail.com>
2020-12-30 09:47:11 +0000

    update load_vocab

--- load_vocab.py
+++ load_vocab.py
@@ -11,3 +11,13 @@ def load_path_index(path):
             res[data[0]] = index
             index +=1
     return res
+
+def load_path_value(path):
+    lines = read_from_txt.read_from_split(path)
+    res = {}
+    for data in lines:
+        if data[0] not in res and data[0]!='':
+            res[data[0]] = data[1]
+    return res
+
+
